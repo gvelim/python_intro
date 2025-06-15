520890d11b8c1dc9a8371910866299a590412e01
gvelim <3185416+gvelim@users.noreply.github.com>
2025-06-15 20:35:19 +0100

    finetune
    remove source activate redudant call in post shellhook event

--- flake.nix
+++ flake.nix
@@ -15,7 +15,7 @@
       devShells.${system} = {
         default = pkgs.mkShellNoCC rec {
           name = "Python3 virtual environment";
-          venvDir = "./.venv";
+          venvDir = "./.venv"; # required by venvShellHook()
           buildInputs = with pkgs; [
             python3Packages.python
             # https://nixos.org/manual/nixpkgs/stable/#how-to-consume-python-modules-using-pip-in-a-virtual-environment-like-i-am-used-to-on-other-operating-systems
@@ -24,14 +24,13 @@
             # https://github.com/NixOS/nixpkgs/commit/eba1f794184f6bc08558a6506ed62eada008f37e#diff-7c765cc9768cdae049f8712b4ef719bc94ec8ec2e8df2dd50ddf14c6acd4e580
             python3Packages.venvShellHook
             python3Packages.python-lsp-server
-            python3Packages.boto3
             nil
             nixd
           ];
           shellHook = "venvShellHook";
           postVenvCreation = ''
-            unset SOURCE_DATE_EPOCH
-            source ${venvDir}/bin/activate
+            # unset SOURCE_DATE_EPOCH
+            # source ${venvDir}/bin/activate <-- venvShellHook() already calls this
             # Install project dependencies using pip
             pip install -r requirements.txt
           '';
